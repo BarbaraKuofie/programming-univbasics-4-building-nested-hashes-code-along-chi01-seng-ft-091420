a51a44892a3a71a0fe1ab6b856978d8c542b16c9
Barbara Kuofie <barbara.kuofie@gmail.com>
2020-08-28 22:40:59 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -6,12 +6,12 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         #your hashes here!
-        {:name => "Benvolio"
-        :age => "17"
+        {:name => "Benvolio",
+        :age => "17",
         :attitude => "worried"
         },
-        {:name => "Mercutio"
-        :age => "18"
+        {:name => "Mercutio",
+        :age => "18",
         :attitude => "hot-headed"
         }
       ]
@@ -22,18 +22,15 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
-        {:name => "Steven"
-        :age => "30"
+        {:name => "Steven",
+        :age => "30",
         :attitude => "confused"
         },
-        {:name => "Nurse"
-        :age => "44"
+        {:name => "Nurse",
+        :age => "44",
         :attitude => "worried"
         }
       ]
    }
 }
-
-  
-
 end
